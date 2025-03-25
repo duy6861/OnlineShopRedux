273337977913fcb9180007877e3fdd114de0d6ad
duy6861 <duy6861@gmail.com>
2025-03-25 16:58:05 +0700

    final

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import "./style/navBar.scss"
+import { useSelector } from 'react-redux'
 const NavBar = () => {
+  const { cartTotalQuantity } = useSelector((state) => state.cart)
   return (
     <nav className="nav-bar">
       <Link to="/">
@@ -12,7 +14,7 @@ const NavBar = () => {
             <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4z" />
           </svg>
           <span className="bag-quantity">
-            <span>3</span>
+            <span>{cartTotalQuantity}</span>
           </span>
         </div>
       </Link>
